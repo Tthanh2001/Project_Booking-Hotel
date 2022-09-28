e41a752df7c3aeb5bb54cb986d31aedd03536eb7
Tthanh2001 <77046685+Tthanh2001@users.noreply.github.com>
2022-09-28 19:46:30 +0700

    project

---
+++ Project_PRN211_FALL2022/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Hotel_Web_app_Projet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Web_app_Projet.Controllers
+{
+    public class HomeController : Controller
+    {
+        HotelWebsiteContext context = new();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.RoomTypes = context.RoomTypes.ToList();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Project_PRN211_FALL2022/Models/RoomType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Hotel_Web_app_Projet.Models
+{
+    public partial class RoomType
+    {
+        public RoomType()
+        {
+            Rooms = new HashSet<Room>();
+        }
+
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public int Acreage { get; set; }
+        public int Beds { get; set; }
+        public int Bathrooms { get; set; }
+        public int Capacity { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
